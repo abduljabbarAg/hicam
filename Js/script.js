dc8e59f111ba16c138c8d8912f3aee2974783188
Tu276 <tu276nathan@gmail.com>
2020-08-06 00:21:46 +0300

    about us edit 1

--- Js/script.js
+++ Js/script.js
@@ -1,3 +1,14 @@
 document.querySelector(".hamburger-menu").addEventListener("click", () => {
     document.querySelector(".container").classList.toggle("change");
 });
+
+//############################################################################################ Progress Bar
+(function($){  
+    if($('.progress-line').length){
+        $('.progress-line').appear(function(){
+            var el = $(this);
+            var percent = el.data('width');
+            $(el).css('width',percent+'%');
+        },{accY: 0});
+    }
+}(jQuery));
