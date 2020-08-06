b7aaa8d844d5520abf2d8b3cbb5c887c401291ad
AJ <68082038+abduljabbarAg@users.noreply.github.com>
2020-08-06 07:47:40 +0300

    update script.js

--- Js/script.js
+++ Js/script.js
@@ -6,8 +6,7 @@ $('.main').click(function () {
     $(this).children('.hide').slideToggle(1000);
     $(this).children('.show').toggle(2000);
   });
-  
-=======
+
 document.querySelector(".hamburger-menu").addEventListener("click", () => {
     document.querySelector(".container").classList.toggle("change");
 });
